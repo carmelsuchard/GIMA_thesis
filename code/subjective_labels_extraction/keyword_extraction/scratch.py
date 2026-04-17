092425992372d666569827e4982ec7ac6fb34e28
“Carmel <carmelsuchard@gmail.com>
2026-04-17 11:45:05 +0200

    Scratch

--- code/subjective_labels_extraction/keyword_extraction/scratch.py
+++ code/subjective_labels_extraction/keyword_extraction/scratch.py
@@ -1,10 +1,10 @@
 import pandas as pd
 subject_col = "subject"
 
-GROUND_TRUTH_CSV = r"C:\Users\5298954\Documents\Github_Repos\GIMA_thesis\code\data_cleaning\annotations_summary_manually_edited.csv"
+GROUND_TRUTH_CSV = r"C:\Users\5298954\Documents\Github_Repos\GIMA_thesis\code\data_cleaning\annotations_summary_manually_edited.xlsx"
 PREDICTIONS_CSV = r"C:\Users\5298954\Documents\Github_Repos\GIMA_thesis\code\subjective_labels_extraction\keyword_extraction\keyword_extraction.csv"
 
-df_ref = pd.read_csv(GROUND_TRUTH_CSV, usecols=["thesisID", subject_col], encoding="latin-1", sep=";")
+df_ref = pd.read_excel(GROUND_TRUTH_CSV, usecols=["Thesis_File", subject_col], engine="openpyxl")
 # df_pred = pd.read_csv(PREDICTIONS_CSV, usecols=["thesisID", "subject_predicted"], encoding="latin-1")
 
 print(df_ref)
